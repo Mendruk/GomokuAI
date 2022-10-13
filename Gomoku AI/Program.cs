8fc4e638cf71d45c7e750a45907704f411fdcb14
Artem Mendruk <artemmendruk@gmail.com>
2022-10-13 18:50:38 +0200

    attempt

--- Gomoku AI/Program.cs
+++ Gomoku AI/Program.cs
@@ -1,1 +1,29 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System.Drawing;
+
+namespace GomokuAI
+{
+    class Program
+    {
+        public static Point GetNextWinningTurn(int[,] map, int playerNumber)
+        {
+            return new Point(0, 0);
+        }
+
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                int player, n;
+                int[,] map = new int[100, 100];
+                player = int.Parse(Console.ReadLine());
+                n = int.Parse(Console.ReadLine());
+                for (int i = 1; i <= n; ++i)
+                    for (int j = 1; j <= n; ++j)
+                        map[i, j] = int.Parse(Console.ReadLine());
+
+                GetNextWinningTurn(map, player);
+                Console.WriteLine(GetNextWinningTurn(map, player).X.ToString() + " " + GetNextWinningTurn(map, player).Y.ToString());
+            }
+        }
+    }
+}
