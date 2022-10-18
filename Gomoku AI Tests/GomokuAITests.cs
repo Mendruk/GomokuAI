e00927d285ddbdd28257888ca2db880464f7a61f
Artem Mendruk <artemmendruk@gmail.com>
2022-10-18 22:38:44 +0200

    Attempt 2

--- Gomoku AI Tests/GomokuAITests.cs
+++ Gomoku AI Tests/GomokuAITests.cs
@@ -1,39 +1,41 @@
 ﻿using NUnit.Framework;
 using System.Diagnostics;
 using System.Drawing;
+using static Gomoku_AI.Program;
 
-namespace GomokuAI
+namespace Gomoku_AI_Tests;
+
+[TestFixture]
+public class GomokuAITests
 {
-    [TestFixture]
-    public class GomokuAITests
+    private const int playerNumber = 1;
+    private const int gameMapSize = 15;
+
+
+    [TestCase("0 1", "1 1", "2 1", "3 1", ExpectedResult = "4 1")]
+    [TestCase("1 0", "1 1", "1 2", "1 3", ExpectedResult = "1 4")]
+    [TestCase("0 0", "1 1", "2 2", "3 3", ExpectedResult = "4 4")]
+    [TestCase("0 4", "1 3", "2 2", "3 1", ExpectedResult = "4 0")]
+    [TestCase("0 1", "1 1", "3 1", "4 1", ExpectedResult = "2 1")]
+    public string TestGetNextTurn(params string[] points)
     {
-        const int playerNumber = 1;
-        const int gameMapSize = 15;
         int[,] map = new int[gameMapSize, gameMapSize];
 
-        [TestCase("0 1", "1 1", "2 1", "3 1", ExpectedResult = "4 1")]
-        [TestCase("1 0", "1 1", "1 2", "1 3", ExpectedResult = "1 4")]
-        [TestCase("0 0", "1 1", "2 2", "3 3", ExpectedResult = "4 4")]
-        [TestCase("1 1", "2 1", "4 1", "5 1", ExpectedResult = "3 1")]
-
-        public string TestGetNextWinningTurn(params string[] points)
+        foreach (string point in points)
         {
-            foreach (string point in points)
-            {
-                string[] pointCoordinates = point.Split();
+            string[] pointCoordinates = point.Split();
 
-                map[int.Parse(pointCoordinates[0]), int.Parse(pointCoordinates[1])] = playerNumber;
-            }
-
-            Stopwatch stopwatch = Stopwatch.StartNew();
-
-            Point nextWinningTurn = GetNextWinningTurn(map, playerNumber);
-
-            stopwatch.Stop();
-
-            Assert.IsTrue(stopwatch.Elapsed.Milliseconds < 1000);
-
-            return nextWinningTurn.X.ToString() + " " + nextWinningTurn.Y.ToString();
+            map[int.Parse(pointCoordinates[0]), int.Parse(pointCoordinates[1])] = playerNumber;
         }
+
+        Stopwatch stopwatch = Stopwatch.StartNew();
+
+        Point nextWinningTurn = GetNextTurn(map, playerNumber);
+
+        stopwatch.Stop();
+
+        Assert.IsTrue(stopwatch.Elapsed.Milliseconds < 1000);
+
+        return nextWinningTurn.X + " " + nextWinningTurn.Y;
     }
 }
--- Gomoku AI/Program.cs
+++ Gomoku AI/Program.cs
@@ -1,29 +1,109 @@
 ﻿using System.Drawing;
 
-namespace GomokuAI
+namespace Gomoku_AI;
+
+public class Program
 {
-    class Program
+    public static Point GetNextTurn(int[,] map, int playerNumber)
     {
-        public static Point GetNextWinningTurn(int[,] map, int playerNumber)
+        for (int x = 0; x < map.GetLength(0); x++)
+            for (int y = 0; y < map.GetLength(1); y++)
+                if (IsWinningTurn(map, playerNumber, x, y))
+                {
+                    if (map[x, y] != 0)
+                        continue;
+
+                    return new Point(x, y);
+                }
+
+        return new Point(0, 0);
+    }
+
+    private static void Main(string[] args)
+    {
+        while (true)
         {
-            return new Point(0, 0);
+            int player = int.Parse(Console.ReadLine());
+
+            int n = int.Parse(Console.ReadLine());
+
+            int[,] map = new int[n, n];
+
+            for (int i = 0; i < n; ++i)
+                for (int j = 0; j < n; ++j)
+                    map[i, j] = int.Parse(Console.ReadLine());
+
+            GetNextTurn(map, player);
+            Console.WriteLine(GetNextTurn(map, player).X + " " + GetNextTurn(map, player).Y);
+        }
+    }
+
+    private static bool IsWinningTurn(int[,] map, int playerNumber, int x, int y)
+    {
+        int numberInHorizontalRow = 0;
+
+        for (int i = x - 4; i <= x + 4; i++)
+        {
+            if (i < 0 || i >= map.GetLength(0))
+                continue;
+
+            if (map[i, y] == playerNumber || i == x)
+                numberInHorizontalRow++;
+            else
+                numberInHorizontalRow = 0;
+
+            if (numberInHorizontalRow == 5)
+                return true;
         }
 
-        static void Main(string[] args)
-        {
-            while (true)
-            {
-                int player, n;
-                int[,] map = new int[100, 100];
-                player = int.Parse(Console.ReadLine());
-                n = int.Parse(Console.ReadLine());
-                for (int i = 1; i <= n; ++i)
-                    for (int j = 1; j <= n; ++j)
-                        map[i, j] = int.Parse(Console.ReadLine());
+        int numberInVerticalRow = 0;
 
-                GetNextWinningTurn(map, player);
-                Console.WriteLine(GetNextWinningTurn(map, player).X.ToString() + " " + GetNextWinningTurn(map, player).Y.ToString());
-            }
+        for (int i = y - 4; i <= y + 4; i++)
+        {
+            if (i < 0 || i >= map.GetLength(1))
+                continue;
+
+            if (map[x, i] == playerNumber || i == y)
+                numberInVerticalRow++;
+            else
+                numberInVerticalRow = 0;
+
+            if (numberInVerticalRow == 5)
+                return true;
         }
+
+        int numberInAcross = 0;
+
+        for (int i = x - 4; i <= x + 4; i++)
+        {
+            if (i < 0 || i >= map.GetLength(1))
+                continue;
+
+            if (map[i, i] == playerNumber || (i == x && i == y))
+                numberInAcross++;
+            else
+                numberInAcross = 0;
+
+            if (numberInAcross == 5)
+                return true;
+        }
+
+        //int numberInReverseAcross = 0;
+
+        //for (int i = x - 4; i <= x + 4; i++)
+        //{
+        //    if (i < 0 || i >= map.GetLength(1))
+        //        continue;
+
+        //    if (map[i, i] == playerNumber || (i == x && i == y))
+        //        numberInReverseAcross++;
+        //    else
+        //        numberInReverseAcross = 0;
+
+        //    if (numberInReverseAcross == 5)
+        //        return true;
+        //}
+
+        return false;
     }
 }
