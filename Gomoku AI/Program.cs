a6e1623c5c9ab62464f6c6f0af63d6201232a5a8
Artem Mendruk <artemmendruk@gmail.com>
2022-10-25 18:03:14 +0200

    More attempts

--- Gomoku AI/Program.cs
+++ Gomoku AI/Program.cs
@@ -4,24 +4,6 @@ public class Program
 {
     private const int WinningNumber = 5;
 
-    public static Cell GetNextWinningTurn(Cell[,] cells, int playerNumber, out bool isWinningTurn)
-    {
-        foreach (Cell cell in cells.OfType<Cell>().Where(cell => cell.PlayerNumber == playerNumber))
-        {
-            foreach (Cell adjacentCell in EnumerateAdjacentCells(cell, cells))
-            {
-                if (IsWinningTurn(cells, adjacentCell, playerNumber))
-                {
-                    isWinningTurn = true;
-                    return adjacentCell;
-                }
-            }
-        }
-
-        isWinningTurn = false;
-        return GetDefaultPoint(cells);
-    }
-
     public static Cell GetNextTurn(int[,] map, int playerNumber)
     {
         Cell[,] cells = new Cell[map.GetLength(0), map.GetLength(1)];
@@ -30,64 +12,19 @@ public class Program
             for (int y = 0; y < map.GetLength(1); y++)
                 cells[x, y] = new Cell(x, y, map[x, y]);
 
-        int enemyNumber = playerNumber == 1 ? 2 : 1;
+        IEnumerable<(Cell cell, int turnToWin)> cellsEnumerable = Array.Empty<(Cell cell, int turnToWin)>();
 
-        List<(Cell cell, int turnToWin)> cellList = new();
+        foreach (Cell cell in cells.OfType<Cell>().Where(cell => cell.PlayerNumber == playerNumber))
+        {
+            cellsEnumerable = cellsEnumerable.Concat(GetEnumerableOfCellAndTurnToWins(cells, cell, playerNumber));
+        }
 
-        foreach (Cell cell in cells.OfType<Cell>().Where(cell => cell.PlayerNumber == playerNumber ))
-            GetNextCell(1, cell, out bool hasWinning);
-
-        if (cellList.Count > 0)
-            return cellList
+        if (cellsEnumerable.Any())
+            return cellsEnumerable
                 .MinBy(cell => cell.turnToWin)
                 .cell;
 
         return GetDefaultPoint(cells);
-
-        void GetNextCell(int turnNumber, Cell cell, out bool hasWinningTurn)
-        {
-            hasWinningTurn = false;
-
-            foreach (Cell adjacentCell in EnumerateAdjacentCells(cell, cells).Where(c => c.PlayerNumber == 0))
-            {
-                Cell cellToReturn = GetNextWinningTurn(cells, playerNumber, out bool isWinningTurn);
-
-                if (isWinningTurn)
-                {
-                    if (turnNumber == 1)
-                        cellList.Add((cellToReturn, turnNumber));
-
-                    hasWinningTurn = true;
-                    return;
-                }
-
-                if (turnNumber > WinningNumber)
-                {
-                    hasWinningTurn = false;
-                    return;
-                }
-
-                turnNumber++;
-
-                adjacentCell.PlayerNumber = playerNumber;
-
-                GetNextCell(turnNumber, adjacentCell, out hasWinningTurn);
-
-                adjacentCell.PlayerNumber = 0;
-                turnNumber--;
-                if (hasWinningTurn)
-                    cellList.Add((adjacentCell, turnNumber));
-            }
-        }
-    }
-
-    private static IEnumerable<Cell> EnumerateAdjacentCells(Cell cell, Cell[,] cells)
-    {
-        for (int dx = -1; dx <= 1; dx++)
-            for (int dy = -1; dy <= 1; dy++)
-                if (cell.X + dx >= 0 && cell.X + dx < cells.GetLength(0) &&
-                    cell.Y + dy >= 0 && cell.Y + dy < cells.GetLength(1))
-                    yield return cells[cell.X + dx, cell.Y + dy];
     }
 
     private static Cell GetDefaultPoint(Cell[,] cells)
@@ -103,77 +40,199 @@ public class Program
         return cells[0, 0];
     }
 
-    private static bool IsWinningTurn(Cell[,] cells, Cell cell, int playerNumber)
+    private static IEnumerable<(Cell cell, int turnToWin)> GetEnumerableOfCellAndTurnToWins(Cell[,] cells, Cell targetCell, int playerNumber)
     {
-        int x = cell.X;
-        int y = cell.Y;
-        int numberInHorizontalRow = 0;
+        int enemyNumber = playerNumber == 1 ? 2 : 1;
+        int x = targetCell.X;
+        int y = targetCell.Y;
         int searchOffset = WinningNumber - 1;
 
-        for (int i = x - searchOffset; i <= x + searchOffset; i++)
+        if (GetRightMaskCells().All(cell => cell.PlayerNumber != enemyNumber))
         {
-            if (i < 0 || i >= cells.GetLength(0))
-                continue;
-
-            if (cells[i, y].PlayerNumber == playerNumber || i == x)
-                numberInHorizontalRow++;
-            else
-                numberInHorizontalRow = 0;
-
-            if (numberInHorizontalRow == WinningNumber)
-                return true;
+            foreach (Cell cell in GetRightMaskCells()
+                         .Where(cell => cell.PlayerNumber == 0))
+            {
+                yield return (cell, GetRightMaskCells()
+                    .Where(cell => cell.PlayerNumber == 0)
+                    .Count());
+            }
         }
 
-        int numberInVerticalRow = 0;
-
-        for (int i = y - searchOffset; i <= y + searchOffset; i++)
+        if (GetLeftMaskCells().All(cell => cell.PlayerNumber != enemyNumber))
         {
-            if (i < 0 || i >= cells.GetLength(1))
-                continue;
-
-            if (cells[x, i].PlayerNumber == playerNumber || i == y)
-                numberInVerticalRow++;
-            else
-                numberInVerticalRow = 0;
-
-            if (numberInVerticalRow == WinningNumber)
-                return true;
+            foreach (Cell cell in GetRightMaskCells()
+                         .Where(cell => cell.PlayerNumber == 0))
+            {
+                yield return (cell, GetRightMaskCells()
+                    .Where(cell => cell.PlayerNumber == 0)
+                    .Count());
+            }
         }
 
-        int numberInAcross = 0;
-
-        for (int i = x - searchOffset; i <= x + searchOffset; i++)
+        if (GetDownMaskCells().All(cell => cell.PlayerNumber != enemyNumber))
         {
-            if (i < 0 || i >= cells.GetLength(1))
-                continue;
-
-            if (cells[i, i].PlayerNumber == playerNumber || (i == x && i == y))
-                numberInAcross++;
-            else
-                numberInAcross = 0;
-
-            if (numberInAcross == WinningNumber)
-                return true;
+            foreach (Cell cell in GetRightMaskCells()
+                         .Where(cell => cell.PlayerNumber == 0))
+            {
+                yield return (cell, GetRightMaskCells()
+                    .Where(cell => cell.PlayerNumber == 0)
+                    .Count());
+            }
         }
 
-        int numberInReverseAcross = 0;
-
-        for (int i = x - searchOffset; i <= x + searchOffset; i++)
+        if (GetUpMaskCells().All(cell => cell.PlayerNumber != enemyNumber))
         {
-            if (i < 0 || i >= cells.GetLength(1) ||
-                searchOffset - i < 0 || searchOffset - i >= cells.GetLength(1))
-                continue;
-
-            if (cells[i, searchOffset - i].PlayerNumber == playerNumber || (i == x && searchOffset - i == y))
-                numberInReverseAcross++;
-            else
-                numberInReverseAcross = 0;
-
-            if (numberInReverseAcross == WinningNumber)
-                return true;
+            foreach (Cell cell in GetRightMaskCells()
+                         .Where(cell => cell.PlayerNumber == 0))
+            {
+                yield return (cell, GetRightMaskCells()
+                    .Where(cell => cell.PlayerNumber == 0)
+                    .Count());
+            }
         }
 
-        return false;
+        if (GetRightDownMaskCells().All(cell => cell.PlayerNumber != enemyNumber))
+        {
+            foreach (Cell cell in GetRightMaskCells()
+                         .Where(cell => cell.PlayerNumber == 0))
+            {
+                yield return (cell, GetRightMaskCells()
+                    .Where(cell => cell.PlayerNumber == 0)
+                    .Count());
+            }
+        }
+
+        if (GetLeftUpMaskCells().All(cell => cell.PlayerNumber != enemyNumber))
+        {
+            foreach (Cell cell in GetRightMaskCells()
+                         .Where(cell => cell.PlayerNumber == 0))
+            {
+                yield return (cell, GetRightMaskCells()
+                    .Where(cell => cell.PlayerNumber == 0)
+                    .Count());
+            }
+        }
+
+        if (GetRightUpMaskCells().All(cell => cell.PlayerNumber != enemyNumber))
+        {
+            foreach (Cell cell in GetRightMaskCells()
+                         .Where(cell => cell.PlayerNumber == 0))
+            {
+                yield return (cell, GetRightMaskCells()
+                    .Where(cell => cell.PlayerNumber == 0)
+                    .Count());
+            }
+        }
+
+        if (GetLeftDownMaskCells().All(cell => cell.PlayerNumber != enemyNumber))
+        {
+            foreach (Cell cell in GetRightMaskCells()
+                         .Where(cell => cell.PlayerNumber == 0))
+            {
+                yield return (cell, GetRightMaskCells()
+                    .Where(cell => cell.PlayerNumber == 0)
+                    .Count());
+            }
+        }
+
+        //
+
+        IEnumerable<Cell> GetRightMaskCells()
+        {
+            if (x + searchOffset >= cells.GetLength(0))
+                yield break;
+
+            for (int i = x; i <= x + searchOffset; i++)
+            {
+                yield return cells[i, y];
+            }
+        }
+
+        IEnumerable<Cell> GetLeftMaskCells()
+        {
+            if (x - searchOffset < 0)
+                yield break;
+
+            for (int i = x; i >= x - searchOffset; i--)
+            {
+                yield return cells[i, y];
+            }
+        }
+
+        IEnumerable<Cell> GetDownMaskCells()
+        {
+            if (y + searchOffset >= cells.GetLength(1))
+                yield break;
+
+            for (int i = y; i <= y + searchOffset; i++)
+            {
+                yield return cells[x, i];
+            }
+        }
+
+        IEnumerable<Cell> GetUpMaskCells()
+        {
+            if (y - searchOffset < 0)
+                yield break;
+
+            for (int i = y; i >= y - searchOffset; i--)
+            {
+                yield return cells[x, i];
+            }
+        }
+
+        IEnumerable<Cell> GetRightDownMaskCells()
+        {
+            if (x + searchOffset >= cells.GetLength(0) ||
+                y + searchOffset >= cells.GetLength(1))
+                yield break;
+
+            for (int i = x; i <= x + searchOffset; i++)
+            {
+                yield return cells[i, i];
+            }
+        }
+
+        IEnumerable<Cell> GetLeftUpMaskCells()
+        {
+            if (x - searchOffset < 0 ||
+                y - searchOffset < 0)
+                yield break;
+
+            for (int i = x; i >= x - searchOffset; i--)
+            {
+                yield return cells[i, y];
+            }
+        }
+
+        IEnumerable<Cell> GetRightUpMaskCells()
+        {
+            if (x + searchOffset >= cells.GetLength(0) ||
+                y + searchOffset >= cells.GetLength(1) ||
+                x - searchOffset < 0 ||
+                y - searchOffset < 0)
+                yield break;
+
+            for (int i = x; i <= x + searchOffset; i++)
+            {
+                yield return cells[i, searchOffset - i];
+            }
+        }
+
+        IEnumerable<Cell> GetLeftDownMaskCells()
+        {
+            if (x + searchOffset >= cells.GetLength(0) ||
+                y + searchOffset >= cells.GetLength(1) ||
+                x - searchOffset < 0 ||
+                y - searchOffset < 0)
+                yield break;
+
+            for (int i = x; i >= x - searchOffset; i--)
+            {
+                yield return cells[i, searchOffset - i];
+            }
+        }
+
     }
 
     private static void Main(string[] args)
